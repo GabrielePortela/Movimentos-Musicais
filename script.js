dd6581b0d05ae4ace698d7b8a79c227c00d86885
Gabriele Vitória dos Santos Portela <164439944+GabrielePortela@users.noreply.github.com>
2025-03-31 22:53:06 +0000

    31/03/25

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function(){
+  let tamanhoAtualFonte = 1;
+
+  const botaoAumentarFonte = document.getElementById("aumentar-fonte");
+  const botaoDiminuirFonte =
+  document.getElementById("diminuir-fonte");
+
+  botaoAumentarFonte.addEventListener("click", function(){
+  tamanhoAtualFonte += 0.1;
+  document.body.fontSize = `$(tamanhoAtualFonte + "rem"});
+  
+});
